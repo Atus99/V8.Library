882ed07759f85318ec11d4b62ab2b25c4c2295cc
CYBEREYE\tu.phamvan <tu.phamvan@cybereye.vn>
2022-02-22 10:49:41 +0700

    Update code

--- V8.Web/Controllers/Controller System/GroupPermissionController.cs
+++ V8.Web/Controllers/Controller System/GroupPermissionController.cs
@@ -13,7 +13,7 @@ namespace V8.Web.Controllers.Controller_System
     [ApiExplorerSettings(IgnoreApi = true)]
     public class GroupPermissionController : BaseController
     {
-        // quyền
+        // Quyền hệ thống
         [HasPermission((int)EnumModule.Code.S9030, new int[] { (int)EnumPermission.Type.Read })]
         public IActionResult Index()
         {
